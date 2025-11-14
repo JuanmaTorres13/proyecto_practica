0a88825604d29089db4afbbede1d503ccca1f1f3
JuanmaTorres13 <juanma.torres.89@gmail.com>
2025-11-14 14:28:15 +0100

    Evento

---
+++ src/main/java/com/eventzone/eventzone/model/EventoCine.java
@@ -0,0 +1,10 @@
+package com.eventzone.eventzone.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "eventos_cine")
+public class EventoCine extends Evento {
+	
+}
